dcba05bf597435b3fdd07686a05828d5bf519587
Gerisha <75563368+Gerisha@users.noreply.github.com>
2023-01-10 21:17:23 +0530

    Update delete_values.py

--- delete_values.py
+++ delete_values.py
@@ -1,19 +1,20 @@
-import pandas as pd 
-import csv 
+import pandas as pd
+import csv
 
-df = pd.read_csv("total_star.csv")
-del df["Luminosity"]
-del df["Star_name"]
-del df["Distance"]
-del df["Mass"]
-del df["Radius"]
-del df["Unnamed: 0"]
-del df["Unnamed: 6"]
-df.dropna()
+df = pd.read_csv("total_stars.csv")
 
+print(df)
 
-df.to_csv("main.csv")
-df.reset_index(drop=True,inplace = True)
+print(df.columns)
 
-print(df.shape)
-print(list(df))
+df.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
+
+final_data = df.dropna()
+
+final_data.reset_index(drop=True,inplace = True)
+
+print(final_data)
+
+final_data.to_csv('main.csv')
+
+final_data.info()
